591328f78d80dd2007c112bcae3793e4b88b844d
lucynguyen-vmo <lucy.nguyen@vmodev.com>
2022-06-15 13:27:36 +0700

    research vue3 15/6/2022

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Head @add-task="addTask" />
+  <ul>
+    <li v-for="(task, index) in listTask" :key="index">
+      {{ task.title }}
+      <button @click="removeTask(task.id)">Delete</button>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+import axios from "axios";
+import Head from "./components/Head.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Head,
+  },
+  setup() {
+    const listTask = ref([]);
+
+    // get list task
+    const getAllTask = async () => {
+      try {
+        const res = await axios.get(
+          "https://62a955243b31438554315c7d.mockapi.io/api/v1/todos"
+        );
+        listTask.value = res.data;
+      } catch (error) {
+        console.log("erorr", error.response);
+      }
+    };
+
+    getAllTask();
+
+    //add task
+    const addTask = async (event) => {
+      try {
+        const newTask = await axios.post(
+          "https://62a955243b31438554315c7d.mockapi.io/api/v1/todos",
+          event
+        );
+        listTask.value = [...listTask.value, newTask.data];
+      } catch (error) {
+        console.log("error", error.response);
+      }
+    };
+
+    // remove task
+    const removeTask = async (idTask) => {
+      try {
+        const deleteTask = await axios.delete(
+          `https://62a955243b31438554315c7d.mockapi.io/api/v1/todos/${idTask}`
+        );
+        listTask.value = listTask.value.filter(
+          (task) => task.id !== deleteTask.data.id
+        );
+      } catch (error) {
+        console.log("error", error.response);
+      }
+    };
+
+    return { listTask, addTask, removeTask };
   },
 };
 </script>
---
+++ src/components/Head.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 @click="logHello">qưeqweqeqwe</h1>
+  <form @submit="checkVal">
+    <input type="text" v-model="title" />
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup(props, context) {
+    console.log("props", props);
+    console.log("context", context);
+    const title = ref("");
+
+    const checkVal = (event) => {
+      event.preventDefault();
+      const newTask = {
+        id: Date.now(),
+        title: title.value,
+        completed: false,
+      };
+      context.emit("add-task", newTask);
+      title.value = "";
+    };
+
+    return { title, checkVal };
+  },
+  methods: {
+    logHello() {
+      console.log("ggggg");
+    },
+  },
+};
+</script>
